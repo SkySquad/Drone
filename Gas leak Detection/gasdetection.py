3b8ea1d9eaf93178583de29819b4040f19ac9dbd
SkySquad <114928216+SkySquad@users.noreply.github.com>
2022-10-06 00:24:42 +0300

    Update gasdetection.py

--- Gas leak Detection/gasdetection.py
+++ Gas leak Detection/gasdetection.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Gas leak detection using thermal cameras
+""" This is  an artificial intelligence model to detect gas leak detection using bayesian decision theory
 
 """
 
